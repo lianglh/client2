2becb984618b879e71b27a40a24c176f9e9c68eb
e <xxxxx@qq.com>
2020-08-19 11:21:35 +0800

    代码提交

--- src/main/java/com/dandan/client2/Client2Application.java
+++ src/main/java/com/dandan/client2/Client2Application.java
@@ -1,11 +1,13 @@
 package com.dandan.client2;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan(basePackages = {"com.dandan.client2.mapper"})
 public class Client2Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/dandan/client2/servisce/AdminService.java
@@ -0,0 +1,13 @@
+package com.dandan.client2.servisce;
+
+
+import com.dandan.client2.entity.Admin;
+
+public interface AdminService {
+
+
+
+    Admin inserAdmin(Admin admin) throws Exception;
+
+
+}
