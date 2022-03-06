95226d948c0a46fe6d55c7920efaa938540186d4
dandan <1025028771@qq.com>
2022-03-06 14:29:48 +0800

    qqq

--- src/main/java/com/dandan/client2/Client2Application.java
+++ src/main/java/com/dandan/client2/Client2Application.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class Client2Application {
+public class  Client2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Client2Application.class, args);
